f7177e2c28c2723ed024c59d41ed3da59cf7ed82
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2022-12-04 13:50:35 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,12 +1,15 @@
 // declare class
-class Shape{
-  constructor (){
-    this.color="green";
-  }
-  drawShape() {
-  }
-  calculateArea(){
-  }
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+
+    }
 }
+
 // export class using module.exports
-module.exports=Shape;
+module.exports = Shape;
